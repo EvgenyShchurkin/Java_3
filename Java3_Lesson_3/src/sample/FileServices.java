7d6dd4fc59738742ebade7c92cf2ca71aac31563
EvgenyShchurkin <jeyshch@gmail.com>
2021-04-25 23:16:38 +0300

    ThirdHomework

---
+++ Java3_Lesson_3/src/sample/FileServices.java
@@ -0,0 +1,130 @@
+package sample;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class FileServices {
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+
+    //метод для записи истории в файл на клиенте
+    public static void writeHistory(String msg, String nickname) {
+        StringBuilder sb = new StringBuilder("src/sample/history/"+nickname).append("-history.txt");
+        String filePath= sb.toString();
+        sb=null;
+        File file = new File(filePath);
+        try {
+            if(!file.exists()) {
+                file.createNewFile();
+            }
+            String tmp=null;
+            br=new BufferedReader(new FileReader(file));
+            bw = new BufferedWriter(new FileWriter(file,true));
+            if ((tmp = br.readLine()) != null) {
+                bw.newLine();
+            }
+            bw.write(msg);
+            bw.close();
+            br.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //метод для отображения поледних {n} сообщений после аутентификации
+    //работает только для английской раскладки
+    public static String getHistoryFromFile(String nickname, int n){
+        File file = new File("src/sample/history/"+nickname+"-history.txt");
+        String result = null;
+        if(!file.exists()){
+            return result;
+        }
+        try {
+            RandomAccessFile raf = new RandomAccessFile(file, "r");
+            long length = file.length() - 1;
+            int readLines = 0;
+            StringBuilder sb = new StringBuilder();
+            for (long i = length; i >= 0 ; i--) {
+                raf.seek(i);
+                char c = (char) raf.read();
+
+                if (c == '\n'){
+                    readLines++;
+                    if (readLines == n){
+                        break;
+                    }
+                }
+                sb.append(c);
+            }
+            sb.reverse();
+            result=sb.toString();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+    //подсчитывает количество строк в файле, делался в тестовых целях
+    public static int getStringCount(String nickname){
+        File file = new File("src/sample/history/"+nickname+"-history.txt");
+        int result=0;
+        if(!file.exists()){
+            return result;
+        }
+
+        try {
+            br = new BufferedReader(new FileReader(file));
+            String str=null;
+            while((str=br.readLine())!=null){
+                result++;
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    //метод для отображения поледних {n} сообщений после аутентификации
+    //в начале нужно прочитать всю историю сообщений
+    public static String getLastMessages(int numberLastMsg, String nickname){
+        List<String> msgList = new LinkedList<>();
+        File file = new File("src/sample/history/"+nickname+"-history.txt");
+        if(!file.exists()){
+            return null;
+        }
+        else{
+            try {
+                br=new BufferedReader(new FileReader(file));
+                String str =null;
+                int msgCount =0;
+                while((str=br.readLine())!=null){
+                    if ((++msgCount) > numberLastMsg) {
+                        msgList.remove(0);
+                    }
+                    msgList.add(str);
+                }
+                br.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i =0;i<msgList.size();i++){
+                sb.append(msgList.get(i)).append("\n");
+            }
+            return sb.toString();
+        }
+
+    }
+
+    public static void close(){
+        try {
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
