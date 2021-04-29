8cf2e5354133590250ead431e3239fe4bf745f38
EvgenyShchurkin <jeyshch@gmail.com>
2021-04-29 11:24:00 +0300

    InitialCommit

---
+++ Java3_Lesson4/Chat/src/sample/Controller.java
@@ -0,0 +1,210 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+
+public class Controller{
+    private Socket client;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private String nickname;
+
+    @FXML
+    TextArea mainTextArea;
+    @FXML
+    TextField sendTextField, loginField;
+    @FXML
+    HBox bottomPanel,upperPanel;
+    @FXML
+    ListView<String> userList ;
+    @FXML
+    PasswordField passField;
+    @FXML
+    VBox vBox;
+
+    private boolean isAuthorized;
+
+    public void setAuthorized(boolean auth){
+        this.isAuthorized=auth;
+
+        if(!isAuthorized){
+            upperPanel.setVisible(true);
+            upperPanel.setManaged(true);
+            bottomPanel.setVisible(false);
+            bottomPanel.setManaged(false);
+            userList.setManaged(false);
+            userList.setVisible(false);
+            vBox.setVisible(false);
+            vBox.setManaged(false);
+        }
+        else{
+            upperPanel.setVisible(false);
+            upperPanel.setManaged(false);
+            bottomPanel.setVisible(true);
+            bottomPanel.setManaged(true);
+            userList.setManaged(true);
+            userList.setVisible(true);
+            vBox.setVisible(true);
+            vBox.setManaged(true);
+        }
+    }
+
+
+    @FXML
+    public void sendMsg() {
+        try{
+            if(!sendTextField.getText().isEmpty()) {
+                if(sendTextField.getText().equals("/getHistory")){
+                    mainTextArea.clear();
+                }
+                out.writeUTF(sendTextField.getText());
+                sendTextField.clear();
+                sendTextField.requestFocus();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    public void exitAction() {
+        Platform.exit();
+    }
+    @FXML
+    public void clearTextArea() {
+        mainTextArea.clear();
+    }
+    @FXML
+    public void cpToBuffer() {
+        String copyText = mainTextArea.getText();
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+        ClipboardContent content = new ClipboardContent();
+        content.putString(copyText);
+        clipboard.setContent(content);
+    }
+
+    public void connect() {
+        try {
+            client = new Socket("localhost", 8089);
+            in = new DataInputStream(client.getInputStream());
+            out = new DataOutputStream(client.getOutputStream());
+            Thread newClient = new Thread(()->{
+                try {
+                    while(true) {
+                        String msg = in.readUTF();
+                        String []tokens= msg.split(" ", 2);
+                        if("/authOk".equals(tokens[0])){
+                            setNickname(tokens[1]);
+                            mainTextArea.clear();
+                            setAuthorized(true);
+//                            long pointer =FileServices.getFilePointer(getNickname(),2);
+//                            System.out.println(pointer);
+//                            if(FileServices.getLastMessagesUsePointer(2,pointer+1,getNickname())!=null) {
+//                                mainTextArea.appendText(FileServices.getLastMessagesUsePointer(2,pointer+1,getNickname()));
+//                            }
+                            if(FileServices.getHistoryFromFile(getNickname(),5)!=null) {
+                                mainTextArea.appendText(FileServices.getHistoryFromFile(getNickname(),5));
+                                mainTextArea.appendText("\n");
+                            }
+                            break;
+                        }
+
+                        else {
+                            mainTextArea.appendText(msg + "\n");
+                        }
+                    }
+
+                    while(true) {
+                        String msg = in.readUTF();
+                        if("/ServerClosed".equals(msg)){
+                            break;
+                        }
+                        else if(msg.startsWith("/clients_list")){
+                            String[] tokens = msg.split(" ");
+                            Platform.runLater(()->{
+                                userList.getItems().clear();
+                                for (int i= 1;i<tokens.length;i++){
+                                    userList.getItems().add(tokens[i]);
+                                }
+                            });
+
+                        }
+                        else {
+                            FileServices.writeHistory(msg, nickname);
+                            mainTextArea.appendText(msg + "\n");
+                        }
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+                finally{
+
+                    try {
+                        client.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    clearTextArea();
+                    setAuthorized(false);
+                }
+            });
+            newClient.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            mainTextArea.appendText("Connection refused");
+        }
+
+    }
+
+    private void setNickname(String nickname) {
+        this.nickname=nickname;
+    }
+    private String getNickname(){
+        return this.nickname;
+    }
+
+    public void tryToAuth(ActionEvent actionEvent) {
+        if(client == null || client.isClosed()){
+            connect();
+        }
+        try{
+            out.writeUTF("/auth "+loginField.getText()+" "+passField.getText());
+            loginField.clear();
+            passField.clear();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void disconnect() {
+        if (client != null ) {
+            try {
+                out.writeUTF("/end");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (client != null) {
+                    client.close();
+                }
+            } catch (IOException | NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            Platform.exit();
+        }
+    }
+}
---
+++ Java3_Lesson4/Chat/src/server/ClientHandler.java
@@ -0,0 +1,189 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientHandler {
+    Socket client;
+    Server server;
+    DataInputStream in;
+    DataOutputStream out;
+    String nickname;
+    List<String> blackList;
+
+
+    public ClientHandler(Server server, Socket client){
+        try {
+            this.server=server;
+            this.client = client;
+            in = new DataInputStream(client.getInputStream());
+            out = new DataOutputStream(client.getOutputStream());
+            new Thread(()->{
+                try {
+                    // auth login pass
+                    while (true){
+                        client.setSoTimeout(20*1000);
+                        String str = in.readUTF();
+                        if(str.startsWith("/auth")){
+                            String [] tokens = str.split(" ");
+                            String nick = AuthService.getNicknameByLoginAndPassword(tokens[1],tokens[2]);
+                            if(nick!=null){
+                                if(!server.isNickBusy(nick)) {
+                                    client.setSoTimeout(0);
+                                    sendMsg("/authOk " +nick);
+                                    setNickname(nick);
+                                    setBlacklist(nick);
+                                    server.subscribe(this);
+                                    System.out.printf("Client [%s] has just connected to server\n", client.getInetAddress());
+                                    break;
+                                }
+                                else {
+                                    sendMsg("This user ["+nick+"] has already logged in");
+                                }
+                            }
+                            else{
+                                System.out.printf("Client [%s] trying to connected to server\n", client.getInetAddress());
+                                sendMsg("/Auth Wrong");
+                            }
+                        }
+                    }
+
+                    while (true) {
+                        String str = in.readUTF();
+                        if(str.startsWith("/")) {
+                            if ("/end".equals(str)) {
+                                out.writeUTF("/ServerClosed");
+                                System.out.printf("Client [%s] disconnected\n", client.getInetAddress());
+                                break;
+                            }
+                            if(str.startsWith("/black ")){
+                                blackListOperations(str);
+                            }
+                            if(str.equals("/getHistory")){
+                                List<String> historyList=AuthService.getHistoryListByNickname(nickname);
+                                sendMsg("----History Loaded----");
+                                for (int i = 0; i < historyList.size() ; i++) {
+                                    sendMsg(historyList.get(i));
+                                }
+                                historyList.clear();
+                            }
+                            if (str.startsWith("/getBlack")){
+                                sendMsg("Your blacklist: ");
+                                for (int i = 0; i < blackList.size() ; i++) {
+                                    sendMsg(blackList.get(i));
+                                }
+                            }
+                        }
+
+                        else if(str.startsWith("@")) {
+                            String[] partsMsg = str.split(" ",2);
+                            server.sendPrivateMsg(this, partsMsg[0].substring(1, partsMsg[0].length()),partsMsg[1]);
+                        }
+
+                        else {
+                            System.out.println(str);
+//                            AuthService.addRecordToDB(nickname,str);
+                            server.broadcastMsg(nickname+": "+str, this);
+                        }
+                    }
+                }catch (SocketTimeoutException e){
+                    System.out.println("Timeout "+client.getInetAddress());
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    closeHandler();
+                }
+
+            }).start();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void blackListOperations(String str) {
+        String[] partsMsg=str.split(" ");
+        if(partsMsg.length!=2){
+            sendMsg("Service message: If you want to add someone to your blacklist you need to specify " +
+                    "a username correctly after command /blacklist {username}.");
+        }
+        else {
+            partsMsg = str.split(" ", 2);
+            if(nickname.equals(partsMsg[1])){
+                sendMsg("Service message: You can't add yourself to blacklist.");
+                return;
+            }
+            if (!AuthService.getBlackListByNickname(nickname).contains(partsMsg[1])) {
+                if (AuthService.addUserToBlacklist(nickname, partsMsg[1]) == 1) {
+                    blackList.add(partsMsg[1]);
+                    sendMsg("You added user [" + partsMsg[1] + "] to blacklist");
+                } else {
+                    sendMsg("Something went wrong during adding user [" + partsMsg[1] + "] to your blacklist");
+                }
+            } else {
+                if (AuthService.deleteUserFromBlacklist(nickname, partsMsg[1]) == 1) {
+                    blackList.remove(partsMsg[1]);
+                    sendMsg("User [" + partsMsg[1] + "] not in your blacklist anymore");
+                } else {
+                    sendMsg("Something went wrong during deleting user [" + partsMsg[1] + "] from your blacklist");
+                }
+            }
+        }
+    }
+
+    private void setBlacklist(String nick) {
+        this.blackList = AuthService.getBlackListByNickname(nick);
+    }
+
+
+    private void setNickname(String nick) {
+        this.nickname=nick;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+    public List<String> getBlackList(){
+        return blackList;
+    }
+
+    public void sendMsg(String msg){
+        try {
+            out.writeUTF(msg);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void closeHandler(){
+        try{
+            if(in!=null) {
+                in.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        try{
+            if(out!=null) {
+                out.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        try {
+            if(client!=null||!client.isClosed()) {
+                client.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        server.unsubscribe(this);
+    }
+
+
+}
